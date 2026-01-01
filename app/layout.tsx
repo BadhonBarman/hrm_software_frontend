0f3828ac434f16ba2e5c22e794c14a7d73ea7838
Badhon Barman <58289031+BadhonBarman@users.noreply.github.com>
2026-01-01 06:33:27 +0600

    toaster container location fixed, right to centered

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AuthProvider>
           {children}
-          <Toaster position="top-right" richColors />
+          <Toaster position="top-center" richColors />
         </AuthProvider>
       </body>
     </html>
